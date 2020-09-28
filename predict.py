e3378d3df774d7b46bc7a8d1e1e7528ab298e309
Mohit ASHAR <mohitashar1995@gmail.com>
2020-09-29 00:55:39 +0200

    Initial logic set

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@
 #    By: mashar <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/25 05:35:49 by mashar            #+#    #+#              #
-#    Updated: 2020/09/25 06:45:59 by mashar           ###   ########.fr        #
+#    Updated: 2020/09/28 23:04:37 by mashar           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 try:
@@ -21,4 +21,5 @@ if(float(value_list[0]) == 0 and float(value_list[1]) == 0):
     print("Model not trained, price will be zero")
 else:
     print("Theta 0 is " + value_list[0] + " and Theta 1 is " + value_list[1])
+    print("Price is: " + str(mileage * float(value_list[1]) + float(value_list[0])))
 f.close()
--- train.py
+++ train.py
@@ -6,14 +6,39 @@
 #    By: mashar <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/25 06:06:01 by mashar            #+#    #+#              #
-#    Updated: 2020/09/25 06:46:01 by mashar           ###   ########.fr        #
+#    Updated: 2020/09/29 00:36:22 by mashar           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+import csv
 
-theta_1 = 15
-theta_2 = 15
+theta_0 = 0.0
+theta_1 = 0.0
+
+with open('data.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+learning_rate = 0.00000000001
+m = len(data[1:])
+for i in range(20):
+    sum_t0 = 0.0
+    sum_t1 = 0.0
+    for item in data[1:]:
+        #print(sum_t0)
+        sum_t0 = sum_t0 + (theta_0 + (theta_1 * float(item[0])) - float(item[1]))
+        sum_t1 = sum_t1 + (theta_0 + (theta_1 * float(item[0])) - float(item[1])) * float(item[0])
+    temp_theta_0 = sum_t0 / m
+    temp_theta_1 = sum_t1 / m
+    theta_0 = theta_0 - (learning_rate * temp_theta_0)
+    theta_1 = theta_1 - (learning_rate * temp_theta_1)
+    print(temp_theta_0)
+    print(temp_theta_1)
+    print(theta_0)
+    print(theta_1)
+    print("\n")
+
 
 f = open("values.txt", "w")
-f.write(str(theta_1) + "\n" + str(theta_2))
+f.write(str(theta_0) + "\n" + str(theta_1))
 f.close()
 
