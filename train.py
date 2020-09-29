c4fc7926ccac493377c4a99239de020ff24f1fb9
Mohit ASHAR <mohitashar1995@gmail.com>
2020-09-29 23:07:26 +0200

    graph and LR added

--- train.py
+++ train.py
@@ -6,10 +6,13 @@
 #    By: mashar <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/25 06:06:01 by mashar            #+#    #+#              #
-#    Updated: 2020/09/29 00:36:22 by mashar           ###   ########.fr        #
+#    Updated: 2020/09/29 23:06:42 by mashar           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 theta_0 = 0.0
 theta_1 = 0.0
@@ -18,27 +21,52 @@ with open('data.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-learning_rate = 0.00000000001
+learning_rate = 0.005
 m = len(data[1:])
-for i in range(20):
+min_km = min(float(x[0]) for x in data[1:])
+max_km = max(float(x[0]) for x in data[1:])
+
+max_price = max(float(x[1]) for x in data[1:])
+min_price = min(float(x[1]) for x in data[1:])
+
+for i in range(100000): #set as 100000
     sum_t0 = 0.0
     sum_t1 = 0.0
     for item in data[1:]:
         #print(sum_t0)
-        sum_t0 = sum_t0 + (theta_0 + (theta_1 * float(item[0])) - float(item[1]))
-        sum_t1 = sum_t1 + (theta_0 + (theta_1 * float(item[0])) - float(item[1])) * float(item[0])
-    temp_theta_0 = sum_t0 / m
-    temp_theta_1 = sum_t1 / m
-    theta_0 = theta_0 - (learning_rate * temp_theta_0)
-    theta_1 = theta_1 - (learning_rate * temp_theta_1)
-    print(temp_theta_0)
-    print(temp_theta_1)
-    print(theta_0)
-    print(theta_1)
-    print("\n")
+        item_0 = (float(item[0]) - min_km) / (max_km - min_km)
+        item_1 = (float(item[1]) - min_price) / (max_price - min_price)
+        sum_t0 = sum_t0 + (theta_0 + (theta_1 * item_0) - item_1)
+        sum_t1 = sum_t1 + (theta_0 + (theta_1 * item_0) - item_1) * item_0
+    temp_theta_0 = learning_rate * 1/m * sum_t0
+    temp_theta_1 = learning_rate * 1/m * sum_t1
+    theta_0 -= temp_theta_0
+    theta_1 -= temp_theta_1
 
+    #plt_theta_0 = theta_0 * (max_price - min_price) + min_price + (theta_1 * min_km * (max_price - min_price)) / (max_km - min_km)
+    #plt_theta_1 = theta_1 * (max_price - min_price) / (max_km - min_km)
+    #plt.figure()
+    #for item in data[1:]:
+    #    plt.scatter(float(item[0]),float(item[1]))
+    #plot_x = np.linspace(0, 250000, 250000)
+    #plot_y = plt_theta_1 * plot_x + plt_theta_0
+    #plt.plot(plot_x, plot_y, '-r')
+    #plt.xlabel("Mileage")
+    #plt.ylabel("Price")
+    #plt.show()
+    #time.sleep(0.001)
+    #plt.close()
+theta_0 = theta_0 * (max_price - min_price) + min_price + (theta_1 * min_km * (max_price - min_price)) / (max_km - min_km)
+theta_1 = theta_1 * (max_price - min_price) / (max_km - min_km)
 
 f = open("values.txt", "w")
 f.write(str(theta_0) + "\n" + str(theta_1))
 f.close()
-
+for item in data[1:]:
+    plt.scatter(float(item[0]),float(item[1]))
+plot_x = np.linspace(0, 250000, 250000)
+plot_y = theta_1 * plot_x + theta_0
+plt.plot(plot_x, plot_y, '-r')
+plt.xlabel("Mileage")
+plt.ylabel("Price")
+plt.show()
